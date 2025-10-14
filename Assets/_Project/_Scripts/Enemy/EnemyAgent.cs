17c55da952e1915bdb23f351ef78d9272168a046
ingolfTHEcapitan <vitaly.litov4encko2017@gmail.com>
2025-10-14 19:21:44 +1000

    feat: добавлен противник перемещающийся в случайном направлении

---
+++ Assets/_Project/_Scripts/Enemy/EnemyAgent.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace _Project._Scripts.Enemy
+{
+    public class EnemyAgent : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent _agent;
+        
+        [SerializeField] private float _minRoamingDistance;
+        [SerializeField] private float _maxRoamingDistance;
+        [SerializeField] private float _maxRoamingTime = 3f;
+        
+        private float _roamingTime;
+        private Vector3 _currentPosition;
+        private Vector3 _roamingPosition;
+        
+        private void Update() => 
+            Roaming();
+
+        private void Roaming()
+        {
+            _roamingTime -= Time.deltaTime;
+
+            if (_roamingTime <= 0)
+            {
+                _currentPosition = transform.position;
+                _roamingPosition = GetRoamingPosition(_currentPosition);
+                _agent.SetDestination(_roamingPosition);
+                _roamingTime = _maxRoamingTime;
+            }
+        }
+
+        private Vector3 GetRoamingPosition(Vector3 currentPosition) => 
+            currentPosition + GetRandomDirection() * Random.Range(_minRoamingDistance, _maxRoamingDistance);
+
+        private Vector3 GetRandomDirection() => 
+            new(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+    }
+}
