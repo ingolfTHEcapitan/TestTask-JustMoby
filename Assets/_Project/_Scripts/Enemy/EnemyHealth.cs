bd16ff8f20b9f369cbffb30c1d13803ae47d4d09
ingolfTHEcapitan <vitaly.litov4encko2017@gmail.com>
2025-10-14 19:23:03 +1000

    feat: механика здоровья противников и его отображение над ними

---
+++ Assets/_Project/_Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,45 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace _Project._Scripts.Enemy
+{
+    public class EnemyHealth : MonoBehaviour
+    {
+        [SerializeField] private float _maxHealth = 100f;
+        [SerializeField] private Slider _healthSlider;
+        [SerializeField] private TextMeshProUGUI _healthText;
+        
+        public event Action OnHealthChanged;
+        
+        public float Current {get; private set;}
+
+        private void Awake()
+        {
+            Current = _maxHealth;
+            OnHealthChanged +=  UpdateView;
+        }
+        
+        private void Start()
+        {
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            _healthSlider.value = Current / _maxHealth;
+            _healthText.text = $"{Current}/{_maxHealth}";
+        }
+        
+        public void TakeDamage(float damage)
+        {
+            Current -= Mathf.Max(damage, 0f);
+            Current = Mathf.Clamp(Current, 0f, _maxHealth);
+            OnHealthChanged?.Invoke();
+        }
+    }
+}
+
+
---
+++ Assets/_Project/_Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace _Project._Scripts.UI
+{
+    public class LookAtCamera: MonoBehaviour
+    {
+        private Camera _camera;
+
+        private void Start() => 
+            _camera = Camera.main;
+
+        private void LateUpdate()
+        {
+            Quaternion rotation = _camera.transform.rotation;
+            transform.LookAt(transform.position + rotation * Vector3.forward, rotation * Vector3.up);
+        }
+    }
+}
